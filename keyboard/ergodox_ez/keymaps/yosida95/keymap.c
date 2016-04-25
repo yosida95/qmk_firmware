6a5e14f8c8473b73b2ea3c27a2644c199c30b78c
yosida95 <kohei@yosida95.com>
2016-04-25 12:41:24 +0900

    update keymap
    * replace KC_HYPER with LALT(KC_SPACE) for Alfread2
    * replace KC_MEH with LGUI(KC_SPACE) for IME

--- keyboard/ergodox_ez/keymaps/yosida95/keymap.c
+++ keyboard/ergodox_ez/keymaps/yosida95/keymap.c
@@ -14,8 +14,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * ,--------------------------------------------------.           ,--------------------------------------------------.
  * | `      |   1  |   2  |   3  |   4  |   5  | LEFT |           | RIGHT|   6  |   7  |   8  |   9  |   0  |   =    |
  * |--------+------+------+------+------+-------------|           |------+------+------+------+------+------+--------|
- * | Tab/L1 |   Q  |   W  |   E  |   R  |   T  | Hyper|           | Meh  |   Y  |   U  |   I  |   O  |   P  |  -/L1  |
- * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
+ * | Tab    |   Q  |   W  |   E  |   R  |   T  |LAlt +|           |LGUI +|   Y  |   U  |   I  |   O  |   P  |  -/L1  |
+ * |--------+------+------+------+------+------| Space|           | Space|------+------+------+------+------+--------|
  * | LCtrl  |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |   ;  |  Enter |
  * |--------+------+------+------+------+------|  [   |           |  ]   |------+------+------+------+------+--------|
  * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |   /  |'/RShift|
@@ -34,21 +34,21 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 // Otherwise, it needs KC_*
 [BASE] = KEYMAP(  // layer 0 : default
         // left hand
-        KC_GRV,         KC_1,   KC_2,   KC_3,   KC_4,   KC_5,KC_LEFT,
-        LT(SYMB,KC_TAB),KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,KC_HYPR,
-        KC_LCTRL,       KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
-        KC_LSFT,        KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_LBRC,
-        KC_LALT,        KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY,
-                                                         KC_MPRV, KC_MNXT,
-                                                                 KC_EJCT,
-                                                KC_SPACE,KC_LGUI,KC_ESC,
+        KC_GRV,  KC_1,   KC_2,   KC_3,   KC_4,   KC_5,KC_LEFT,
+        KC_TAB,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,LALT(KC_SPACE),
+        KC_LCTRL,KC_A,   KC_S,   KC_D,   KC_F,   KC_G,
+        KC_LSFT, KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,KC_LBRC,
+        KC_LALT, KC_MUTE,KC_VOLD,KC_VOLU,KC_MPLY,
+                                                 KC_MPRV, KC_MNXT,
+                                                          KC_EJCT,
+                                        KC_SPACE,KC_LGUI,KC_ESC,
 
         // right hand
-        KC_RGHT,KC_6,KC_7,   KC_8,   KC_9,  KC_0,   KC_EQL,
-        KC_MEH, KC_Y,KC_U,   KC_I,   KC_O,  KC_P,   LT(SYMB,KC_MINS),
-                KC_H,KC_J,   KC_K,   KC_L,  KC_SCLN,KC_ENT,
-        KC_RBRC,KC_N,KC_M,   KC_COMM,KC_DOT,KC_SLSH,MT(MOD_RSFT,KC_QUOT),
-                     KC_LEFT,KC_DOWN,KC_UP, KC_RGHT,MT(MOD_RALT,KC_BSLS),
+        KC_RGHT,       KC_6,KC_7,   KC_8,   KC_9,  KC_0,   KC_EQL,
+        LGUI(KC_SPACE),KC_Y,KC_U,   KC_I,   KC_O,  KC_P,   LT(SYMB,KC_MINS),
+                       KC_H,KC_J,   KC_K,   KC_L,  KC_SCLN,KC_ENT,
+        KC_RBRC,       KC_N,KC_M,   KC_COMM,KC_DOT,KC_SLSH,MT(MOD_RSFT,KC_QUOT),
+                            KC_LEFT,KC_DOWN,KC_UP, KC_RGHT,MT(MOD_RALT,KC_BSLS),
         KC_HOME,KC_END,
         KC_PGUP,
         KC_PGDN,KC_BSPACE,KC_SPACE
